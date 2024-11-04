a0de662961fc8ef082641819fbf7e4ec05d7a391
Stefan Seunarine <127210112+JalenLT@users.noreply.github.com>
2024-11-03 21:32:30 -0400

    Made Updates to Renderer

--- GameEngine/Renderer.h
+++ GameEngine/Renderer.h
@@ -3,6 +3,8 @@
 #define RENDERER_H
 
 #include "Observer.h"
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 #include "glm/gtc/type_ptr.hpp"
@@ -34,6 +36,76 @@ public:
 			FragColor = vec4(1.0, 0.5, 0.2, 1.0);
 		}
 	)";
+
+	unsigned int VAO, VBO, EBO;
+	std::vector<float> vertices;
+	std::vector<unsigned int> indices;
+
+	void loadModel(const std::string& path) {
+		Assimp::Importer importer;
+		const aiScene* scene = importer.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs);
+		if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode) {
+			std::cout << "Error from ASSIMP: " << importer.GetErrorString() << std::endl;
+			return;
+		}
+
+		processNode(scene->mRootNode, scene);
+	}
+
+	void processNode(aiNode* node, const aiScene* scene) {
+		for (unsigned int i = 0; i < node->mNumMeshes; i++) {
+			aiMesh* mesh = scene->mMeshes[node->mMeshes[i]];
+			processMesh(mesh, scene);
+		}
+
+		for (unsigned int i = 0; i < node->mNumChildren; i++) {
+			processNode(node->mChildren[i], scene);
+		}
+	}
+
+	void processMesh(aiMesh* mesh, const aiScene* scene) {
+		for (unsigned int i = 0; i < mesh->mNumVertices; i++) {
+			vertices.push_back(mesh->mVertices[i].x);
+			vertices.push_back(mesh->mVertices[i].y);
+			vertices.push_back(mesh->mVertices[i].z);
+
+			if (mesh->mNormals) {
+				vertices.push_back(mesh->mNormals[i].x);
+				vertices.push_back(mesh->mNormals[i].y);
+				vertices.push_back(mesh->mNormals[i].z);
+			}
+		}
+
+		for (unsigned int i = 0; i < mesh->mNumFaces; i++) {
+			aiFace face = mesh->mFaces[i];
+			for (unsigned int j = 0; j < face.mNumIndices; j++) {
+				indices.push_back(face.mIndices[j]);
+			}
+		}
+
+		// Setup OpenGL buffers
+		glGenVertexArrays(1, &VAO);
+		glGenBuffers(1, &VBO);
+		glGenBuffers(1, &EBO);
+
+		glBindVertexArray(VAO);
+
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), &vertices[0], GL_STATIC_DRAW);
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
+
+		// Position attribute
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(0);
+
+		// Normal attribute
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+		glEnableVertexAttribArray(1);
+
+		glBindVertexArray(0);
+	}
 };
 
 #endif
